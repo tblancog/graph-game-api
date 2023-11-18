c2b67adec7b8b67c82839f50c2ef1073e7d2f8e9
Tony Blanco <tblanco@codepwr.com>
2023-11-17 23:01:13 -0300

    feat: add query endpoints for graphql

---
+++ index.js
@@ -0,0 +1,59 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+export const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Review: {
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  // typeDefs --definitions
+  typeDefs,
+  // resolvers
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
